d462625da7a5ae31fd4ac423ac7c2e7e2c860783
RobsonBrazJunior <robsonbrazjunior@gmail.com>
2022-12-26 15:32:17 -0400

    Configuração do Context

---
+++ OpenFood/OpenFood.Infra.Data/Context/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OpenFood.Domain.Models;
+
+namespace OpenFood.Infra.Data.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+        }
+    }
+}

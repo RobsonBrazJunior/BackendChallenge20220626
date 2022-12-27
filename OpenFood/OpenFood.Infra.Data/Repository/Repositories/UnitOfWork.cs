bb370821c8d49000022be8fab9e60dbda7012ff8
RobsonBrazJunior <robsonbrazjunior@gmail.com>
2022-12-27 12:53:24 -0400

    Criação da classe UnitOfWork para centralizar os repositórios

---
+++ OpenFood/OpenFood.Infra.Data/Repository/Repositories/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using OpenFood.Infra.Data.Context;
+using OpenFood.Infra.Data.Repository.Interfaces;
+
+namespace OpenFood.Infra.Data.Repository.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationContext _context;
+        private readonly IProductRepository _product;
+
+        public IProductRepository Product { get { return _product; } }
+
+        public UnitOfWork(ApplicationContext context)
+        {
+            _context = context;
+            _product = new ProductRepository(context);
+        }
+
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}

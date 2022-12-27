3cb6efb9637e31c5cf81ae7d5e9e10281e685323
RobsonBrazJunior <robsonbrazjunior@gmail.com>
2022-12-27 14:08:52 -0400

    Criação da classe de serviço do Produto

---
+++ OpenFood/OpenFood.Application/Services/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using OpenFood.Application.Interfaces;
+using OpenFood.Domain.Models;
+using OpenFood.Infra.Data.Repository.Interfaces;
+
+namespace OpenFood.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Product GetById(int id) => _unitOfWork.Product.GetById(id);
+
+        public IEnumerable<Product> GetAll() => _unitOfWork.Product.GetAll();
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}

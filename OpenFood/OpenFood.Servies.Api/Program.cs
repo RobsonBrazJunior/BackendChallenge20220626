20b7852e99bcee52905778e042a9b2555eab8e05
RobsonBrazJunior <robsonbrazjunior@gmail.com>
2022-12-26 20:14:04 -0400

    Ajustes da configuração da ConnectionString

--- OpenFood/OpenFood.Servies.Api/Program.cs
+++ OpenFood/OpenFood.Servies.Api/Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using OpenFood.Infra.Data.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddDbContext<ApplicationContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        x => x.EnableRetryOnFailure(2, TimeSpan.FromSeconds(5), null).MigrationsHistoryTable("_version_migration"));
+});
 
 builder.Services.AddControllers();
 

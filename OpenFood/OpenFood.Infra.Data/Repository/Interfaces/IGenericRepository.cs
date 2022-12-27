9d29f5fee191a74769ec3e3195559cb12a69f1d8
RobsonBrazJunior <robsonbrazjunior@gmail.com>
2022-12-27 10:45:09 -0400

    Criação da interface para o repositório genérico

---
+++ OpenFood/OpenFood.Infra.Data/Repository/Interfaces/IGenericRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenFood.Infra.Data.Repository.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+    }
+}
